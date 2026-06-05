da3d6e000009b8ab024550215a29e63dace05149
mayfau <3946nv@gmail.com>
2026-06-05 17:21:29 +0300

    Move admin password to environment variables

--- src/database.py
+++ src/database.py
@@ -1,8 +1,18 @@
+import os
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "sqlite+aiosqlite:///./database.db"
+# 1. Читаем URL из переменной окружения Render. 
+# Если её нет (например, запускаешь локально), сработает твой старый SQLite
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///./database.db")
 
+# 2. Корректируем префикс для асинхронной библиотеки asyncpg
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
+elif DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+
+# 3. Создаем движок с динамическим URL
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 async_session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
+import os
 import sys
 from fastapi import FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
-from pydantic import BaseModel  # Не забудь импортировать BaseModel для проверки пароля
+from pydantic import BaseModel
 
 from src.database import engine, Base
 from src.paintings.models import PaintingModel 
@@ -24,7 +25,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# --- БЛОК АВТОРИЗАЦИИ ---
+#АВТОРИЗАЦИЯ
+
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "nelli")
 
 class LoginRequest(BaseModel):
     password: str
@@ -32,15 +35,12 @@ class LoginRequest(BaseModel):
 
 @app.post("/auth/login")
 async def login(data: LoginRequest):
-    if data.password == "nelli":
-        # Возвращаем токен, который фронтенд сохранит в localStorage
+    if data.password == ADMIN_PASSWORD:
         return {"access_token": "secret-art-token-2026", "token_type": "bearer"}
     
     raise HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Неверный секретный пароль!"
     )
-
-# ------------------------
 
 app.include_router(paintings_router, prefix="/paintings", tags=["Paintings"])
